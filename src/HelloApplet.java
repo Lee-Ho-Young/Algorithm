fea7aedcdafb2d7c169c224f7c6d85b35005a9d4
Lee-Ho-Young <flghdud1@gmail.com>
2015-09-25 21:45:11 +0900

    코밋

--- src/HelloApplet.java
+++ src/HelloApplet.java
@@ -1,13 +1,10 @@
-
-public class HelloApplet 
+public class HelloApplet
 {
-	
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
-		byte byte1 = 10;
-		byte byte2 = 20;
-		byte byte3 = (byte)(byte1 + byte2);
-		System.out.println(byte3);
+		String str = "Fuck";
+		
+		System.out.println(str.length());
 	}
-
+	
 }
---
+++ src/MismatchedBrackets.java
@@ -0,0 +1,70 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class MismatchedBrackets 
+{
+	public static void main(String args[])
+	{
+		Scanner scanner = new Scanner(System.in);
+		int count; // counting the test cases
+		count = Integer.parseInt(scanner.nextLine()); // store the number of test
+		
+		if(count >= 1){} //if count value is valid then, do nothing
+		else
+		{
+			System.out.println("Wrong input");
+			return;
+		}
+			
+		while(count > 0)
+		{
+			count--;
+			
+			String brackets = scanner.nextLine();
+			int index = 0;
+			boolean flag = true;
+			Stack<Character> stack = new Stack<Character>();
+		
+			while(index <= (brackets.length()-1)) //index starts from 0
+			{
+				if(brackets.charAt(index) == ')')
+				{
+					if(stack.pop() != '(')
+					{
+						flag = false;
+						break;
+					}
+				}
+				else if(brackets.charAt(index) == '}')
+				{
+					if(stack.pop() != '{')
+					{
+						flag = false;
+						break;
+					}
+				}
+				else if(brackets.charAt(index) == ']')
+				{
+					if(stack.pop() != '[')
+					{
+						flag = false;
+						break;
+					}
+				}
+				else
+				{
+					stack.push(brackets.charAt(index));
+				}
+				
+				index++;			
+			}
+			
+			if(flag)
+				System.out.println("Yes");	
+			else
+				System.out.println("No");
+		
+		}
+		System.out.println("The end of Program");	
+	}
+}
