13371b5eb71190b9612f9bd13a149e83403ad707
rich <rich@DESKTOP-TALV2OB>
2017-03-21 01:22:40 +0900

    nextInt버전

--- src/RockFestival.java
+++ src/RockFestival.java
@@ -4,46 +4,31 @@ public class RockFestival {
 	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		String temp1;
-		int temp2;
-		String[] temp3;
+		/*사용할 변수 정의*/
+		int cases = 0; // testCase 갯수
 		
-		// Case의 갯수만큼 돌리겠다.
-		temp2 = Integer.parseInt(sc.nextLine());
-		for(int i = 0; i < temp2; i++ ) {
+		/*입력을 받아들일  Scanner 변수*/
+		Scanner sc = new Scanner(System.in);
+		
+		/*입력받은 Case의 갯수만큼 돌리겠다*/
+		cases = sc.nextInt();
+		
+		for(int i = 0; i < cases; i++ ) {
 			
-			temp1 = sc.nextLine();
-			temp3 = temp1.split(" ");
+			/*set variable*/
+			int N = sc.nextInt(); //대여 가격을 알고 있는 N일
+			int L = sc.nextInt(); //대여기간 동안의 하루평균 대여가격을 확인해 봐야할 최소 대여기간 L
+			int[] cost = new int[N];
 			
-			// 대여 가격을 알고 있는 N일
-			int N = Integer.parseInt(temp3[0]);
-			
-			// 대여기간 동안의 하루평균 대여가격을 확인해 봐야할 최소 대여기간 L
-			int L = Integer.parseInt(temp3[1]);
-			
-			temp1 = sc.nextLine();
-			temp3 = temp1.split(" ");
-			int[] cost = new int[temp3.length];
-			
-			// N일 동안의 하루대여 가격을 costs 배열에 저장
-			for(int j=0; j<cost.length; j++) {	
-				cost[j] = Integer.parseInt(temp3[j]);
-			}
+			for(int j=0; j<N; j++)
+				cost[j] = sc.nextInt();	
 				
 			// 체크해야 할 최소 대여일과 주어진 최대일 사이에서의 가격 체크
 			double minAvgCost = Double.MAX_VALUE;
-			double temp4;
-			for(int j=L; j<=N; j++) {
-				
-				temp4 = average_cost(j, cost);
-				
-				//System.out.println("Log level2 : " + temp4);
-				
-				if(temp4 < minAvgCost)
-					minAvgCost = temp4;
-			}	
 			
+			for(int j=L; j<=N; j++)
+				minAvgCost = Math.min(minAvgCost, average_cost(j, cost));
+						
 			System.out.println(minAvgCost);
 		}	
 	}
@@ -55,18 +40,15 @@ public class RockFestival {
 		
 		//몇 번의 계산을 해야 하는지 알아보자
 		for(int i=0; i<(cost.length-days+1); i++) {
-			double sum = 0.0;
-			double temp1 = 0.0;
+			double sum = 0;
+			double minTemp = 0;
 			
-			for(int j=i; j<i+days; j++) {
+			for(int j=i; j<i+days; j++) 
 				sum += cost[j];
-			}
 			
-			temp1 = sum / days;
-			//System.out.println("Log level1 : " + temp1);
-			if(temp1 < avgCost)
-				avgCost = temp1;
-		}
+			minTemp = sum / days;
+			avgCost = Math.min(avgCost, minTemp);
+			}
 		
 		return avgCost;
 	}
